6dea8ed5c95a92835d4424b6788b4b799551f502
ThanhDuy <thanhduy@hoaiphuc.com>
2023-11-28 15:56:57 +0700

    fix

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { onMounted, ref } from "vue";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import {onMounted, ref} from "vue";
 import axios from "axios";
+
 const data = ref<any>({});
-onMounted(async () => {
+const selected = ref<string>('');
+const renderModel3D = (data: any) => {
+  data.value = data
   const ele = document.getElementById("three");
-  const ele_left = document.getElementById("left");
-  const ele_right = document.getElementById("right");
+  ele!.innerHTML = ''
   if (ele) {
     const scene = new THREE.Scene();
     // set background scene
     scene.background = new THREE.Color(0xffffff);
     // setup default camera
     const camera = new THREE.PerspectiveCamera(
-      40,
-      ele?.offsetWidth / ele?.offsetHeight,
-      0.1,
-      1000
+        40,
+        ele?.offsetWidth / ele?.offsetHeight,
+        0.1,
+        1000
     );
 
     const renderer = new THREE.WebGLRenderer();
@@ -25,22 +27,12 @@ onMounted(async () => {
     renderer.setSize(ele?.offsetWidth, ele?.offsetHeight);
     ele.appendChild(renderer.domElement);
 
-    let res = await axios.get(
-      "http://192.168.1.57:8767/analysis/data?id=6dc7533b-90eb-458a-81a5-ce329b40bb1c"
-    );
     let listMesh: any = [];
 
-    data.value = res.data.data;
-    data.value.faces = data.value.faces.map(it => {
-      return {
-        ...it,
-        isShow: true,
-      };
-    });
     const totalBoundingBox = new THREE.Box3();
     const cameraBoundingBox = new THREE.Box3();
-    for (let index = 0; index < data.value.faces.length; index++) {
-      const face = data.value.faces[index];
+    for (let index = 0; index < data.faces.length; index++) {
+      const face = data.faces[index];
       const geometry = new THREE.BufferGeometry();
       const vertices = new Float32Array(face.vertex);
       const normals = new Float32Array(face.normal);
@@ -49,7 +41,11 @@ onMounted(async () => {
       geometry.setAttribute("normal", new THREE.BufferAttribute(normals, 3));
       geometry.setIndex(new THREE.BufferAttribute(indices, 1));
       geometry.scale(0.1, 0.1, 0.1);
-      let material = new THREE.MeshPhongMaterial({ color: "#ffffff" });
+      let material = new THREE.MeshPhongMaterial({color: "#ffffff"});
+      if (!face.isShow) {
+        material.transparent = true
+        material.opacity = 0.1
+      }
       listMesh[index] = new THREE.Mesh(geometry, material);
       // var geometry_line = new THREE.EdgesGeometry(listMesh[index].geometry);
       // var material_line = new THREE.LineBasicMaterial({
@@ -66,23 +62,23 @@ onMounted(async () => {
       listMesh[index].castShadow = true;
       const cubeBoundingBox = new THREE.Box3().setFromObject(listMesh[index]);
       if (
-        isFinite(cubeBoundingBox.max.x) &&
-        isFinite(cubeBoundingBox.max.y) &&
-        isFinite(cubeBoundingBox.max.z)
+          isFinite(cubeBoundingBox.max.x) &&
+          isFinite(cubeBoundingBox.max.y) &&
+          isFinite(cubeBoundingBox.max.z)
       ) {
         totalBoundingBox.expandByPoint(cubeBoundingBox.min);
         totalBoundingBox.expandByPoint(cubeBoundingBox.max);
       }
-
+      data.value.faces[index].uuid = listMesh[index].uuid
       scene.add(listMesh[index]);
     }
     for (let i = 0; i < listMesh.length; i++) {
       listMesh[i].position.x =
-        (totalBoundingBox.max.x + totalBoundingBox.min.x) / -2;
+          (totalBoundingBox.max.x + totalBoundingBox.min.x) / -2;
       listMesh[i].position.y =
-        Math.abs(totalBoundingBox.max.y + totalBoundingBox.min.y) / 2;
+          Math.abs(totalBoundingBox.max.y + totalBoundingBox.min.y) / 2;
       listMesh[i].position.z =
-        (totalBoundingBox.max.z + totalBoundingBox.min.z) / -2;
+          (totalBoundingBox.max.z + totalBoundingBox.min.z) / -2;
       // const cubeHelper = new THREE.BoxHelper(listMesh[i], 0xffff00);
       // cubeHelper.position.x =
       //   (totalBoundingBox.max.x + totalBoundingBox.min.x) / -2;
@@ -100,19 +96,19 @@ onMounted(async () => {
       }
     }
     const maxObjectSize = Math.max(
-      cameraBoundingBox.max.x,
-      cameraBoundingBox.max.y,
-      cameraBoundingBox.max.z
+        cameraBoundingBox.max.x,
+        cameraBoundingBox.max.y,
+        cameraBoundingBox.max.z
     );
 
     const distance =
-      (maxObjectSize * 2) / Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2);
+        (maxObjectSize * 2) / Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2);
     // console.log("distance", distance);
 
     // Đặt vị trí và hướng nhìn mới cho camera
     camera.position.set(distance, distance, distance);
     // nguồn sáng
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
     directionalLight.position.set(3, 3, 10); // Đặt vị trí của nguồn sáng
     var lightHolder = new THREE.Group();
     lightHolder.add(directionalLight);
@@ -126,19 +122,15 @@ onMounted(async () => {
       // console.log(camera.position);
       const uuids: string[] = [];
       scene.children.forEach((e: any) => uuids.push(e.uuid));
-      console.log(ele_left?.offsetWidth);
-
       mouse.x =
-        (event.clientX /
-          (renderer.domElement.clientWidth +
-            ele_left?.offsetWidth +
-            ele_right?.offsetWidth)) *
+          (event.clientX /
+              (renderer.domElement.clientWidth + (window.innerWidth - renderer.domElement.clientWidth) * 2)) *
           2 -
-        1;
-
+          1;
       mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
       raycaster.setFromCamera(mouse, camera);
       let intersects = raycaster.intersectObjects(scene.children);
+
       if (intersects.length > 0) {
         for (let i = 0; i < intersects.length; i++) {
           const el = intersects[i];
@@ -149,7 +141,6 @@ onMounted(async () => {
                   item.material.color.set("#ffffff");
                 }
               });
-              // console.log(el);
               el.object.material.color.set("#40ed6e");
             }
             return;
@@ -157,6 +148,7 @@ onMounted(async () => {
         }
       }
     }
+
     window.addEventListener("click", onDocumentMouseDown, false);
     // // grid
     // let grid = new THREE.GridHelper(20, 20);
@@ -165,37 +157,64 @@ onMounted(async () => {
     let controls = new OrbitControls(camera, renderer.domElement);
     controls.dampingFactor = 50;
     controls.screenSpacePanning = false;
+
     function animate() {
       requestAnimationFrame(animate);
       lightHolder.quaternion.copy(camera.quaternion);
       renderer.render(scene, camera);
     }
+
     animate();
   }
+}
+
+const handleClick = (index: string) => {
+  data.value.faces = data.value.faces.map((it: any) => {
+
+    return {
+      ...it,
+      isShow: index === it.index ? !it.isShow : it.isShow,
+    };
+  });
+  console.log(data.value.faces)
+  renderModel3D(data.value)
+}
+
+onMounted(async () => {
+  const id = 'b6ed3d7a-5e0f-41d2-99e8-51728ad55966'
+  let res = await axios.get(
+      `http://192.168.1.57:8767/analysis/data?id=${id}`
+  );
+  data.value = res.data.data;
+  data.value.faces = data.value.faces.map((it: any) => {
+    return {
+      ...it,
+      isShow: true,
+    };
+  });
+  renderModel3D(data.value)
 });
 </script>
 
 <template>
-  <div style="display: flex">
+  <div style="display: flex;height: 100vh">
     <div
-      style="width: 15%; height: 100vh; background-color: #ccc; z-index: 1"
-      id="left"
+        style="width: 15%; height: 100vh; background-color: #ccc; z-index: 1;overflow-y: auto;"
+        id="left"
     >
-      <div v-for="item in data.faces" :key="item.id" style="padding: 15px">
+
+      <div v-for="item in data.faces" :key="item.index" style="padding: 15px">
         <div>
           <span style="margin-right: 15px">{{ item.index + 1 }}</span>
+
           <span style="margin-right: 15px">{{
-            item.isShow ? "Show" : "Hidden"
-          }}</span>
-          <button>Change</button>
+              item.isShow ? "Show" : "Hidden"
+            }}</span>
+
+          <button @click="()=>handleClick(item.index)">Change</button>
         </div>
       </div>
     </div>
     <div style="width: 85%; height: 100vh" id="three"></div>
-    <div
-      style="width: 15%; height: 100vh; background-color: #ccc; z-index: 1"
-      id="right"
-    ></div>
-    <!-- <div style="width: 50%; height: 100vh; background-color: beige"></div> -->
   </div>
 </template>
